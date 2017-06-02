1eb8d525d62c55da50479eaf859e818af6a967b9
Tech <jyothsna9966@gmail.com>
2017-06-02 19:29:41 +0530

    First Angular2

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'//import component(oredefined)
+
+@Component({
+  selector:'my-app',
+  template :'<h1>My first Angular -2 Application</h1>'
+})
+export class AppComponent{
+	
+}
